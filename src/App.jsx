3586c8c34702ded09ce54fa5ef411d547ac17504
mujahidul885 <mujahid121019@gmail.com>
2025-06-01 09:28:27 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Textfrorms from "./components/Textfrorms";
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <Textfrorms/>
+    </>
+  );
+}
+
+export default App;
+
+
+
+
